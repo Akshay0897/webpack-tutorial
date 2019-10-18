1a4e2e7dfbfa16e85851f71703e58070dd48f4b6
= <=>
2019-10-18 20:14:29 +0530

    This is first commit of webpack tutorials

---
+++ src/app/app.js
@@ -0,0 +1,19 @@
+import {parseInputs} from './utils/parse-inputs';
+import {inputsAreValid} from './utils/inputs-are-valid';
+
+export const run = (alertService, componentService) => {
+  alertService.hideErrors();
+
+  componentService.onClick(() => {
+    alertService.hideErrors();
+    const inputs = componentService.getInputs();
+    const parsedInputs = parseInputs(...inputs);
+    if (inputsAreValid(...parsedInputs)) {
+      const [numA, numB] = parsedInputs;
+      componentService.setResult(numA + numB);
+    } else {
+      componentService.setResult("");
+      alertService.handleAdditionError(inputs, parsedInputs);
+    }
+  });
+};
